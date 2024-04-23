0c5eb3c759f1d7966dbf21b9e95bc280fa99f75b
JustGabo <ovallegabriel123@gmail.com>
2024-04-22 20:46:57 -0700

    feat: fixing the bug with the broken mascot image on the lesson

--- app/lesson/question-bubble.tsx
+++ app/lesson/question-bubble.tsx
@@ -9,15 +9,15 @@ export const QuestionBubble = ({ question }: Props) => {
   return (
     <div className="flex items-center gap-x-4 mb-6">
       <Image
-        src="/logo.svg"
-        alt="Logo"
+        src="/mascot.svg"
+        alt="Mascot"
         height={60}
         width={60}
         className="hidden lg:block"
       />
       <Image
-        src="/logo.svg"
-        alt="Logo"
+        src="/mascot.svg"
+        alt="Mascot"
         height={40}
         width={40}
         className="lg:hidden block"
